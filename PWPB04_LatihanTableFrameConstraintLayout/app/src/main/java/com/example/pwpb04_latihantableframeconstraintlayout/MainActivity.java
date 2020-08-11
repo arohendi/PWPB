d058b48db869774060f7df80e02d9db494808f78
Agus Rohendi <arohendi.siswa@smkn4bdg.sch.id>
2020-08-11 19:37:29 +0700

    Pertemuan ke-4

---
+++ PWPB04_LatihanTableFrameConstraintLayout/app/src/main/java/com/example/pwpb04_latihantableframeconstraintlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pwpb04_latihantableframeconstraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framelayout2);
+    }
+}
